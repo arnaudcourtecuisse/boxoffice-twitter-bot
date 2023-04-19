38bf416057679e31c809757a0dd4365455ab2642
Arnaud Courtecuisse <arnaud.courtecuisse@webedia-group.com>
2023-04-19 17:59:57 +0200

    fix cron schedule

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,57 +1,53 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "4.5.0"
-    }
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_cloud_scheduler_job" "post_box_office_tweet_job" {
+  name      = "post-box-office-tweet-job"
+  schedule  = "0 9-22 * * 3"
+  time_zone = "Europe/Paris"
+
+  http_target {
+    http_method = "POST"
+    uri         = google_cloud_functions_function.post_box_office_tweet.https_trigger_url
   }
 }
 
-provider "google" {
-  project = "your-project-id"
-  region  = "europe-west1"
-}
+resource "google_cloud_functions_function" "post_box_office_tweet" {
+  name                = "post-box-office-tweet"
+  description         = "Posts a tweet about the latest box office movie using OpenAI and Twitter APIs"
+  runtime             = "nodejs16"
+  timeout             = "60"
+  available_memory_mb = 128
 
-resource "google_storage_bucket" "function_bucket" {
-  name = "your-bucket-name"
-}
-
-resource "google_cloudfunctions_function" "boxoffice_tweet" {
-  name        = "boxoffice-tweet"
-  description = "Posts a tweet about the newly released or best ranked movie of the day"
-
-  runtime = "nodejs16"
-
-  source_archive_bucket = google_storage_bucket.function_bucket.name
-  source_archive_object = "path/to/your/function.zip"
-
-  entry_point = "postBoxOfficeTweet"
+  source_archive_bucket = google_storage_bucket.post_box_office_tweet_bucket.name
+  source_archive_object = google_storage_bucket_object.post_box_office_tweet_object.name
 
   environment_variables = {
-    OPENAI_API_KEY              = "your-openai-api-key"
-    TWITTER_CONSUMER_KEY        = "your-twitter-consumer-key"
-    TWITTER_CONSUMER_SECRET     = "your-twitter-consumer-secret"
-    TWITTER_ACCESS_TOKEN_KEY    = "your-twitter-access-token-key"
-    TWITTER_ACCESS_TOKEN_SECRET = "your-twitter-access-token-secret"
+    TWITTER_CONSUMER_KEY        = var.twitter_consumer_key
+    TWITTER_CONSUMER_SECRET     = var.twitter_consumer_secret
+    TWITTER_ACCESS_TOKEN_KEY    = var.twitter_access_token_key
+    TWITTER_ACCESS_TOKEN_SECRET = var.twitter_access_token_secret
+    OPENAI_API_KEY              = var.openai_api_key
   }
-
-  trigger_http = true
-
-  available_memory_mb = 256
-
-  timeout = "60s"
 }
 
-resource "google_cloud_scheduler_job" "boxoffice_tweet_schedule" {
-  name        = "boxoffice-tweet-schedule"
-  description = "Schedule boxoffice-tweet function to run every hour on Wednesdays between 10am and 11pm CET"
-  schedule    = "0 * * * 3"
-  time_zone   = "CET"
+resource "google_storage_bucket" "post_box_office_tweet_bucket" {
+  name = "post-box-office-tweet-bucket"
 
-  job_target {
-    project_id = google_cloudfunctions_function.boxoffice_tweet.project
-    region     = google_cloudfunctions_function.boxoffice_tweet.region
-    type       = "cloud_function"
-    name       = google_cloudfunctions_function.boxoffice_tweet.name
+  uniform_bucket_level_access = true
+
+  website {
+    main_page_suffix = "index.html"
   }
+}
+
+resource "google_storage_bucket_object" "post_box_office_tweet_object" {
+  name   = "post-box-office-tweet-object"
+  bucket = google_storage_bucket.post_box_office_tweet_bucket.name
+
+  content_type = "application/zip"
+
+  source = "../dist/post-box-office-tweet.zip"
 }
